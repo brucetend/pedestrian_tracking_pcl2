fa6f81c5fcd3dd36fc4b81eb258769d33ce41dce
Xuyang Chen <chenxuyang98@gmail.com>
2019-12-19 16:31:32 +0800

    debuged the error, in which the publisher pulished only one repeated pedestrian position

--- src/scripts/pedestrain_tracker.py
+++ src/scripts/pedestrain_tracker.py
@@ -17,21 +17,23 @@ def callback(data):
     list_of_centroids = list(zip(data.data[::2], data.data[1::2])) # convert list into list of tuples
     #Use the CentroidTracker class to do the association
     pedestrians_dict = ct.update(list_of_centroids)
-    #Initialize p for a pedestrian
-    p = Pedestrian_info()
     #Initialize an array for all pedestrians
     associated_pedestrians = Pedestrians_array()
     
     for key, value in pedestrians_dict.items():
-
+        #Initialization must be inside the loop, otherwise at each time 
+        #p will be overwriten by new value
+        p = Pedestrian_info() 
         p.id = key
         p.x_last, p.y_last, p.x, p.y = value
-        associated_pedestrians.data.append(p)
+        associated_pedestrians.data.append(p) 
+        
+    rospy.loginfo('+'*50)
     
     #Pubilsh the position of all pedestrians as Pedestrians_array
     pub = rospy.Publisher('pedestrians_array', Pedestrians_array, queue_size=10)
     pub.publish(associated_pedestrians.data)
-    rospy.loginfo(associated_pedestrians)
+    rospy.loginfo(associated_pedestrians.data)
 
 
 
